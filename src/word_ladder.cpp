2d15f80cacc7e7759c366f1ad93f3b82989fc310
yixuan lu <z5577113@ad.unsw.edu.au>
2024-06-02 01:28:38 -0700

    implement help function find all possible neighbour

--- src/word_ladder.cpp
+++ src/word_ladder.cpp
@@ -1,4 +1,5 @@
 #include "word_ladder.h"
+#include <vector>
 
 auto word_ladder::read_lexicon(const std::string &path) -> std::unordered_set<std::string> {
 	std::ifstream inFile;
@@ -16,6 +17,22 @@ auto word_ladder::read_lexicon(const std::string &path) -> std::unordered_set<st
 	return lexicon;
 }
 
+/*
+find all possible words in current level
+*/
+auto word_ladder::find_neighbors(const std::string &word, const std::unordered_set<std::string> &lexicon, const std::set<std::string> &visited) -> std::vector<std::string> {
+    std::vector<std::string> neighbors;
+    for (size_t i = 0; i < word.size(); i++) {
+        auto new_word = word;
+        for (char c = 'a'; c <= 'z'; ++c) {
+            new_word[i] = c;
+            if (new_word != word && lexicon.find(new_word) != lexicon.end() && visited.find(new_word) == visited.end()) {
+                neighbors.push_back(new_word);
+            }
+        }
+    }
+    return neighbors;
+}
 
 auto word_ladder::generate(
 	const std::string &from,
@@ -28,5 +45,10 @@ auto word_ladder::generate(
 	std::set<std::string> visited;
 	std::queue<std::vector<std::string>> myqueue;
 	myqueue.push({from});
+	visited.insert(from);
+	while(!myqueue.empty()){
+		std::vector<std::string> curPath = myqueue.front();
+		myqueue.pop();
+	}
 	return {};
 }
--- src/word_ladder.h
+++ src/word_ladder.h
@@ -15,7 +15,7 @@ namespace word_ladder {
 // Given a file path to a newline-separated list of words...
 // Loads those words into an unordered set and returns it.
 auto read_lexicon(const std::string& path) -> std::unordered_set<std::string>;
-
+auto find_neighbors(const std::string &word, const std::unordered_set<std::string> &lexicon, const std::set<std::string> &visited) -> std::vector<std::string>;
 // Given a start word and destination word, returns all the shortest possible paths from the
 // start word to the destination, where each word in an individual path is a valid word per the
 // provided lexicon.
