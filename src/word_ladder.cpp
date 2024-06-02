74a72e972230d30dfbebc6eed8e5971f3d3cb14f
yixuan lu <z5577113@ad.unsw.edu.au>
2024-06-02 09:45:39 -0700

    implement basic structure

--- src/word_ladder.cpp
+++ src/word_ladder.cpp
@@ -1,4 +1,5 @@
 #include "word_ladder.h"
+#include <cstddef>
 
 auto word_ladder::read_lexicon(const std::string& path) -> std::unordered_set<std::string> {
 	std::ifstream inFile;
@@ -15,17 +16,47 @@ auto word_ladder::read_lexicon(const std::string& path) -> std::unordered_set<st
 
 	return lexicon;
 }
+void word_ladder::myDfs(const std::string &word, std::vector<std::string> &seq) {
+	(void) word;
+	(void)	seq;
+}
 
-auto word_ladder::generate(
-	const std::string &from,
-	const std::string &to,
-	const std::unordered_set<std::string> &lexicon
-) -> std::vector<std::vector<std::string>> {
+auto word_ladder::generate(const std::string& from,
+                           const std::string& to,
+                           const std::unordered_set<std::string>& lexicon) -> std::vector<std::vector<std::string>> {
 	//(void)from;
-	(void)to;
-	(void)lexicon;
-	std::set<std::string> visited;
-	std::queue<std::vector<std::string>> myqueue;
+	//(void)to;
+	//(void)lexicon;
+	std::unordered_set<std::string> mylex(lexicon.begin(), lexicon.end());
+	std::queue<std::string> myqueue;
 	myqueue.push({from});
+	mylex.erase(from);
+	wordSteps[from] = 1;
+	startWord = from;
+	std::size_t sizee = from.size();
+	while (!myqueue.empty()) {
+		std::string word = myqueue.front();
+		int steps = wordSteps[word];
+		myqueue.pop();
+		if (word == to)
+			break;
+		for (std::size_t i = 0; i < sizee; i++) {
+			char original = word[i];
+			for (char ch = 'a'; ch <= 'z'; ch++) {
+				word[i] = ch;
+				if (mylex.count(word) > 0) {
+					myqueue.push({word});
+					mylex.erase(word);
+					wordSteps[word] = steps + 1;
+				}
+			}
+			word[i] = original;
+		}
+	}
+	if (wordSteps.find(to) != wordSteps.end()) {
+		std::vector<std::string> seq;
+		seq.push_back(to);
+		myDfs(to, seq);
+	}
 	return {};
 }
--- src/word_ladder.h
+++ src/word_ladder.h
@@ -13,22 +13,31 @@
 #include <algorithm>
 #include <cstdio>
 
-
 namespace word_ladder {
-// Given a file path to a newline-separated list of words...
-// Loads those words into an unordered set and returns it.
-auto read_lexicon(const std::string& path) -> std::unordered_set<std::string>;
-// Given a start word and destination word, returns all the shortest possible paths from the
-// start word to the destination, where each word in an individual path is a valid word per the
-// provided lexicon.
-// Preconditions:
-// - from.size() == to.size()
-// - lexicon.contains(from)
-// - lexicon.contains(to)
-auto generate(
-    const std::string& from,
-    const std::string& to,
-    const std::unordered_set<std::string>& lexicon) -> std::vector<std::vector<std::string>>;
+    // Given a file path to a newline-separated list of words...
+    // Loads those words into an unordered set and returns it.
+    auto read_lexicon(const std::string& path) -> std::unordered_set<std::string>;
+
+    // Given a start word and destination word, returns all the shortest possible paths from the
+    // start word to the destination, where each word in an individual path is a valid word per the
+    // provided lexicon.
+    // Preconditions:
+    // - from.size() == to.size()
+    // - lexicon.contains(from)
+    // - lexicon.contains(to)
+    void myDfs(const std::string &word, std::vector<std::string> &seq);
+    auto generate(
+        const std::string& from,
+        const std::string& to,
+        const std::unordered_set<std::string>& lexicon) -> std::vector<std::vector<std::string>>;
+
+    namespace {
+        std::vector<std::vector<std::string>> ans; // final answer
+        std::unordered_map<std::string, int> wordSteps; // record word steps
+        std::string startWord; // record from
+    }
+
+    void dfs(const std::string &word, std::vector<std::string> &seq);
 } // namespace word_ladder
 
 #endif // COMP6771_WORD_LADDER_H
