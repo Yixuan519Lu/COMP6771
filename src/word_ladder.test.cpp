6d132b3a0eb98d5cbede4edb8b3586bd5ef31ab8
yixuan lu <z5577113@ad.unsw.edu.au>
2024-06-01 19:16:54 -0700

    modify test case for lexicon

--- src/word_ladder.test.cpp
+++ src/word_ladder.test.cpp
@@ -3,8 +3,26 @@
 #include <catch2/catch.hpp>
 
 TEST_CASE("word_ladder::read_lexicon works as expected") {
-	auto const english_lexicon = ::word_ladder::read_lexicon("./english.txt");
-	CHECK(std::size(english_lexicon) == 127142);
+	std::string temp_filename = "temp_lexicon.txt";
+	{
+        std::ofstream temp_file(temp_filename);
+        REQUIRE(temp_file.is_open());
+        temp_file << "adobe\n";
+        temp_file << "blender\n";
+        temp_file << "clion\n";
+        temp_file << "docker\n";
+        temp_file << "eclipse\n";
+    }
+	auto const english_lexicon = ::word_ladder::read_lexicon(temp_filename);
+	std::remove(temp_filename.c_str());
+    REQUIRE(!english_lexicon.empty());
+    CHECK(english_lexicon.size() == 5);
+    CHECK(english_lexicon.find("adobe") != english_lexicon.end());
+    CHECK(english_lexicon.find("blender") != english_lexicon.end());
+    CHECK(english_lexicon.find("clion") != english_lexicon.end());
+    CHECK(english_lexicon.find("docker") != english_lexicon.end());
+    CHECK(english_lexicon.find("eclipse") != english_lexicon.end());
+    CHECK(english_lexicon.find("jetbrains") == english_lexicon.end());
 }
 
 // TEST_CASE("at -> it") {
