1fdf1299bed1983f5ad6db872f978550596af38d
yixuan lu <z5577113@ad.unsw.edu.au>
2024-06-03 01:05:51 -0700

    set indent

--- src/word_ladder.h
+++ src/word_ladder.h
@@ -1,41 +1,40 @@
 #ifndef COMP6771_WORD_LADDER_H
 #define COMP6771_WORD_LADDER_H
 
-#include <string>
-#include <unordered_set>
-#include <cstddef>
-#include <vector>
 #include <unordered_map>
-#include <queue>
-#include <iostream>
-#include <fstream>
-#include <set>
+#include <unordered_set>
 #include <algorithm>
+#include <cstddef>
 #include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
 
 namespace word_ladder {
-    // Given a file path to a newline-separated list of words...
-    // Loads those words into an unordered set and returns it.
-    auto read_lexicon(const std::string& path) -> std::unordered_set<std::string>;
+	// Given a file path to a newline-separated list of words...
+	// Loads those words into an unordered set and returns it.
+	auto read_lexicon(const std::string& path) -> std::unordered_set<std::string>;
 
-    // Given a start word and destination word, returns all the shortest possible paths from the
-    // start word to the destination, where each word in an individual path is a valid word per the
-    // provided lexicon.
-    // Preconditions:
-    // - from.size() == to.size()
-    // - lexicon.contains(from)
-    // - lexicon.contains(to)
-    void myDfs(const std::string from, std::vector<std::string> &seq);
-    auto generate(
-        const std::string& from,
-        const std::string& to,
-        const std::unordered_set<std::string>& lexicon) -> std::vector<std::vector<std::string>>;
+	// Given a start word and destination word, returns all the shortest possible paths from the
+	// start word to the destination, where each word in an individual path is a valid word per the
+	// provided lexicon.
+	// Preconditions:
+	// - from.size() == to.size()
+	// - lexicon.contains(from)
+	// - lexicon.contains(to)
+	void myDfs(const std::string from, std::vector<std::string>& seq);
+	auto generate(const std::string& from,
+	              const std::string& to,
+	              const std::unordered_set<std::string>& lexicon) -> std::vector<std::vector<std::string>>;
 
-    namespace {
-        std::vector<std::vector<std::string>> ans; // final answer
-        std::unordered_map<std::string, int> wordSteps; // record word steps
-        std::string startWord; // record from
-    }
+	namespace {
+		std::vector<std::vector<std::string>> ans; // final answer
+		std::unordered_map<std::string, int> wordSteps; // record word steps
+		std::string startWord; // record from
+	} // namespace
 
 } // namespace word_ladder
 
